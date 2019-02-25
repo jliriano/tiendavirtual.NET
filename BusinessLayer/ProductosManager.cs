0e924fc579a1283377f98da5fbe5c8e5dbd7829c
Joel Liriano <jd.liriano@alumnos.upm.es>
2019-02-25 22:31:03 +0100

    Adding BusinessLayer and Repository logic to add new Productos.

---
+++ BusinessLayer/ProductosManager.cs
@@ -0,0 +1,22 @@
+﻿using Repository.Modelos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer
+{
+    class ProductosManager
+    {
+
+        public void addProducto(string nombre, string descripcion,
+            float precio, int cantidad, string fotoUri)
+        {
+            Productos ProductoManager = new Productos();
+            ProductoManager.Add(nombre, descripcion, precio, cantidad, fotoUri);
+        }
+        
+
+    }
+}
---
+++ Repository/Modelos/Productos.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repository.Modelos
+{
+    public class Productos
+    {
+        P1TiendaVirtualDBEntities Te = new P1TiendaVirtualDBEntities();
+
+        public void Add(string nombre, string descripcion,
+            float precio, int cantidad, string fotoUri)
+        {
+            Producto nuevoProd = new Producto();
+            nuevoProd.nombre = nombre;
+            nuevoProd.descripcion = descripcion;
+            nuevoProd.precio = precio;
+            nuevoProd.cantidad = cantidad;
+            nuevoProd.fotoUri = fotoUri;
+            Te.Productos.Add(nuevoProd);
+        }
+    }
+}
