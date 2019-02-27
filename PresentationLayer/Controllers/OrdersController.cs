d1050409ffcbe64dd1fe24060b84f859c268f67b
Joel Liriano <jd.liriano@alumnos.upm.es>
2019-02-27 09:40:42 +0100

    Adding view for Carrito de Compras (view and remove productos from carrito)

---
+++ PresentationLayer/Controllers/OrdersController.cs
@@ -0,0 +1,44 @@
+﻿using PresentationLayer.Models;
+using Repository.Modelos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PresentationLayer.Controllers
+{
+    public class OrdersController : Controller
+    {
+        // GET: Orders
+        public ActionResult Index()
+        {
+            return RedirectToAction("Cart");
+        }
+
+        //GET: Orders/cart
+        public ActionResult Cart(CarritoCompra cc)
+        {
+            return View(cc);
+        }
+
+        //GET: Orders/QuitarProducto/3
+        public ActionResult QuitarProducto(int? id, CarritoCompra cc)
+        {
+            if (id == null)
+            {
+                return RedirectToAction("Index");
+            }
+            Producto p = cc.Find(x => x.Id == id);
+            cc.Remove(p);
+            return RedirectToAction("Cart");
+        }
+
+        //POST: Orders/Checkout
+        public ActionResult Checkout(CarritoCompra cc)
+        {
+            return View(cc);
+        }
+
+    }
+}
