93ea5cea78615442c46b5303e3d1879c7b7275df
Joel Liriano <jd.liriano@alumnos.upm.es>
2019-02-27 11:30:21 +0100

    Add "Checkout" functionality

--- PresentationLayer/Controllers/OrdersController.cs
+++ PresentationLayer/Controllers/OrdersController.cs
@@ -10,19 +10,21 @@ namespace PresentationLayer.Controllers
 {
     public class OrdersController : Controller
     {
+        private P1TiendaVirtualDBEntities db = new P1TiendaVirtualDBEntities();
+
         // GET: Orders
         public ActionResult Index()
         {
             return RedirectToAction("Cart");
         }
 
-        //GET: Orders/cart
+        // GET: Orders/cart
         public ActionResult Cart(CarritoCompra cc)
         {
             return View(cc);
         }
 
-        //GET: Orders/QuitarProducto/3
+        // GET: Orders/QuitarProducto/3
         public ActionResult QuitarProducto(int? id, CarritoCompra cc)
         {
             if (id == null)
@@ -34,10 +36,63 @@ namespace PresentationLayer.Controllers
             return RedirectToAction("Cart");
         }
 
-        //POST: Orders/Checkout
+        // GET: Orders/Checkout
         public ActionResult Checkout(CarritoCompra cc)
         {
             return View(cc);
+        }
+
+        // POST: Orders/Checkout
+        public ActionResult FinalizarCheckout(string nombreCliente, string emailCliente,
+            string telefonoCliente, string direccionEnvio, CarritoCompra cc)
+        {
+            bool productosDisponibles = true;
+            //Estan disponibles los productos deseados?
+            foreach (Producto item in cc)
+            {
+                Producto prod = db.Productos.Find(item.Id);
+                if (prod.cantidad <= item.cantidad)
+                {
+                    productosDisponibles = false;
+                }
+            }
+            if (productosDisponibles)
+            {
+                //Reducir cantidad disponible de producto en base de datos.
+                foreach (Producto item in cc)
+                {
+                    Producto prod = db.Productos.Find(item.Id);
+                    db.Productos.Find(item.Id).cantidad = prod.cantidad - item.cantidad;
+                    db.SaveChanges();
+                }
+                string detalle = "";
+                decimal subtotal = 0;
+                decimal iva = 0;
+                decimal total = 0;
+                decimal envio = (decimal)5.99;
+                foreach (var item in cc)
+                {
+                    subtotal += (decimal) item.precio;
+                    detalle = detalle + " (x" + item.cantidad + ") " + item.nombre + "[" + item.Id + "]" + " @ " + item.precio;
+                }
+                iva = subtotal * (decimal)0.21;
+                total = subtotal + iva + envio;
+                Pedido nuevoPedido = new Pedido();
+                nuevoPedido.nombre_cliente = nombreCliente;
+                nuevoPedido.email_cliente = emailCliente;
+                nuevoPedido.telefono_cliente = telefonoCliente;
+                nuevoPedido.direccion_cliente = direccionEnvio;
+                nuevoPedido.detalle = detalle;
+                nuevoPedido.subtotal = (float) subtotal;
+                nuevoPedido.iva = (float) iva;
+                nuevoPedido.envio = (float) envio;
+                nuevoPedido.total = (float) total;
+                db.Pedidos.Add(nuevoPedido);
+                db.SaveChanges();
+                cc.LimpiarCarrito(ControllerContext);
+                return View(nuevoPedido);
+            }
+            return View("CheckoutError");
         }
 
     }
--- PresentationLayer/Models/Session/CarritoCompra.cs
+++ PresentationLayer/Models/Session/CarritoCompra.cs
@@ -4,10 +4,16 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Web.Mvc;
 
 namespace PresentationLayer.Models
 {
     public class CarritoCompra : List<Producto>
     {
+        public void LimpiarCarrito(ControllerContext controllerContext)
+        {
+            var cc = new CarritoCompra();
+            controllerContext.HttpContext.Session["carrito"] = cc;
+        }
     }
 }
